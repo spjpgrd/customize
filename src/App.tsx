65559b3f7e09073ea0e9d91d4eb8a5787eb06d20
Ringo Doran <sean.doran@wiretap.com>
2019-04-01 07:15:38 -0400

    🚧 State and props for `fullName`

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,30 @@ import './App.css';
 import EmailSignatureBasic from './components/email-signature-basic';
 // import logo from './logo.svg';
 
-class App extends React.Component {
+
+// tslint:disable-next-line: no-empty-interface
+interface IAppProps {
+
+}
+
+// tslint:disable-next-line: no-empty-interface
+interface IAppState {
+  fullName: string;
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+
+    this.state = {
+      fullName: ""
+    }
+  }
+
   public render() {
     return (
       <>
-        <EmailSignatureBasic />
+        <EmailSignatureBasic fullName={this.state.fullName} />
       </>
     );
   }
--- src/components/email-signature-basic.tsx
+++ src/components/email-signature-basic.tsx
@@ -4,44 +4,52 @@ import * as React from 'react';
 
 // }
 
-// interface IEmailSignatureBasicProps {
+interface IEmailSignatureBasicProps {
+    fullName: string;
+};
 
-// }
+// tslint:disable-next-line: no-empty-interface
+interface IEmailSignatureBasicState {
 
-// interface IEmailSignatureBasicState {
+}
 
-// }
+class EmailSignatureBasic extends React.Component<IEmailSignatureBasicProps, IEmailSignatureBasicState> {
 
-class EmailSignatureBasic extends React.Component<any, any> {
+    constructor(props: IEmailSignatureBasicProps) {
+        super(props);
+
+        this.state = {
+            fulllName: ""
+        }
+    }
 
     public render() {
         return (
             <>
-                <table cellSpacing="0" cellPadding="0" style={{ border: "none" }}>
+                <table cellSpacing="0" cellPadding="0" style={{ border: "none", borderSpacing: "0px" }}>
                     <tbody>
                         <tr>
                             <td style={{ paddingLeft: "0px", paddingTop: "32px", paddingBottom: " 4px", paddingRight: "0px", verticalAlign: "top" }}>
-                                <a href="https://www.linkedin.com/in/austinjmckinley/"><img src="https://www.wiretap.com/hubfs/email-signature/austin-mckinley-64.png" style={{ textDecoration: "none!important", borderBottomWidth: "0", width: "64px", height: "64px", border: "none" }} width="64" height="64" alt="Austin McKinley" title="Austin McKinley" /></a></td>
-                            <tr>
-                                <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "0px", paddingRight: "0px" }}>
-                                    <span style={{ textAlign: "left", color: "#26241F", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontWeight: "bold" }}>Austin McKinley<br /><span style={{ textAlign: "left", marginTop: "0px", marginBottom: "0px", color: "#555759", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontWeight: "normal", fontSize: "10pt", lineHeight: "1.5" }}>Director of Strategic Partnerships | Workplace<br />O: 614.407.9884 • <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="mailto:austin.mckinley@wiretap.com">austin.mckinley@wiretap.com</a></span></span></td>
-                            </tr>
-                            <tr>
-                                <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "0px", paddingRight: "0px" }}>
-                                    <a href="https://wiretap.com?utm_medium=email-sig?utm_source=austin-mckinley"><img src="https://www.wiretap.com/hubfs/email-signature/wiretap-logo-32.png" style={{ width: "123px", height: "32px", border: "0" }} width="123" height="32" alt="Aware" title="Aware" /></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "5px", paddingRight: "0px" }}>
-                                    <p style={{ textAlign: "left", color: "#555759", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }}><b style={{ fontWeight: "bold", color: "#26241F" }}><strong> Aware HQ</strong></b><a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https:// wrtp.me/HQ1directions">📍</a><br />111 Liberty Street | Suite 102 | Columbus, OH 43215<br /><span style={{ textAlign: "left", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }}><a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra', 'Lato', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com?utm_medium=email-sig?utm_source=austin-mckinley">\
-                            }>Wiretap</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com/li?utm_medium=email-sig?utm_source=austin-mckinley">LinkedIn</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="wiretap.com/tw?utm_medium=email-sig?utm_source=austin-mckinley">Twitter</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https:// wiretap.com/fb?utm_medium=email-sig?utm_source=austin-mckinley">Facebook</a> |<a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com/ig?utm_medium=email-sig?utm_source=austin-mckinley">Instagram</a></span></p>
-                                </td >
-                            </tr >
+                                <a href="https://www.linkedin.com/in/austinjmckinley/"><img src="https://www.wiretap.com/hubfs/email-signature/austin-mckinley-64.png" style={{ textDecoration: "none!important", borderBottomWidth: "0", width: "64px", height: "64px", border: "none" }} width="64" height="64" alt={this.props.fullName} title={this.props.fullName} /></a></td>
                         </tr>
+                        <tr>
+                            <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "8px", paddingRight: "0px" }}>
+                                <span style={{ textAlign: "left", color: "#26241F", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontWeight: "bold" }}>{this.props.fullName}<br /><span style={{ textAlign: "left", marginTop: "0px", marginBottom: "0px", color: "#555759", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontWeight: "normal", fontSize: "10pt", lineHeight: "1.5" }}>Director of Strategic Partnerships | Workplace<br />O: 614.407.9884 • <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="mailto:austin.mckinley@wiretap.com">austin.mckinley@wiretap.com</a></span></span></td>
+                        </tr>
+                        <tr>
+                            <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "8px", paddingBottom: "0px", paddingRight: "0px", marginTop: "0px" }}>
+                                <a href="https://wiretap.com?utm_medium=email-sig?utm_source=austin-mckinley"><img src="https://www.wiretap.com/hubfs/email-signature/wiretap-logo-32.png" style={{ width: "123px", height: "32px", border: "0" }} width="123" height="32" alt="Aware" title="Aware" /></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "5px", paddingRight: "0px" }}>
+                                <p style={{ textAlign: "left", color: "#555759", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5", paddingTop: "4px" }}><b style={{ fontWeight: "bold", color: "#26241F" }}><strong> Aware HQ</strong></b><a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https:// wrtp.me/HQ1directions">📍</a><br />111 Liberty Street | Suite 102 | Columbus, OH 43215<br /><span style={{ textAlign: "left", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }}><a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra', 'Lato', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com?utm_medium=email-sig?utm_source=austin-mckinley">Wiretap</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com/li?utm_medium=email-sig?utm_source=austin-mckinley">LinkedIn</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="wiretap.com/tw?utm_medium=email-sig?utm_source=austin-mckinley">Twitter</a> | <a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https:// wiretap.com/fb?utm_medium=email-sig?utm_source=austin-mckinley">Facebook</a> |<a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#F16E25", fontFamily: "'Effra','Lato','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href="https://wiretap.com/ig?utm_medium=email-sig?utm_source=austin-mckinley">Instagram</a></span></p>
+                            </td >
+                        </tr >
                     </tbody>
                 </table>
             </>
-        )
+        );
     }
 }
 

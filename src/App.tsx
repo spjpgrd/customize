fa310fd5fd8b143c1ed3871ed772865e9114966d
Ringo Doran <sean.doran@wiretap.com>
2019-04-01 22:55:53 -0400

    📝 Add titles to a lot of inputs

--- src/App.tsx
+++ src/App.tsx
@@ -52,29 +52,28 @@ class App extends React.Component<IAppProps, IAppState> {
             <legend>Your Details</legend>
             <br /><label htmlFor="honorific-prefix">Honorifix Prefix (optional)</label>
             <br /><span>Examples: Dr., Sir, Queen</span>
-            <br /><input type="text" name="honorific-prefix" autoCorrect="off" title="Dr., Sir, Queen, etc." autoComplete="honorific-prefix" />
+            <br /><input type="text" name="honorific-prefix" autoCorrect="off" title="Dr., Sir, Queen, etc. 👑" autoComplete="honorific-prefix" />
             <br /><label htmlFor="given-name">First Name</label>
-            <br /><input type="text" name="given-name" required={true} autoFocus={true} autoCorrect="off" autoComplete="given-name" />
+            <br /><input type="text" name="given-name" required={true} autoFocus={true} autoCorrect="off" autoComplete="given-name" title="What do they call you? 🤔" />
             <br /><label htmlFor="additional-name">Middle Name (optional)</label>
-            <br /><input type="text" name="additional-name" autoCorrect="off" autoComplete="additional-name" />
+            <br /><input type="text" name="additional-name" autoCorrect="off" autoComplete="additional-name" title="I once worked with someone who had five middle names 🥁" />
             <br /><label htmlFor="family-name">Last Name</label>
-            <br /><input type="text" name="family-name" required={true} autoCorrect="off" autoComplete="family-name" />
+            <br /><input type="text" name="family-name" required={true} autoCorrect="off" autoComplete="family-name" title="Your last name, please 🙃" />
             <br /><label htmlFor="honorific-suffix">Honorific Suffix (optional)</label>
             <br /><span>Examples: Jr., PhD, MBA</span>
-            <br /><input type="text" name="honorific-suffix" autoCorrect="off" autoComplete="honorific-suffix" />
+            <br /><input type="text" name="honorific-suffix" autoCorrect="off" autoComplete="honorific-suffix" title="Suffix or bust 🎰" />
             <br /><label htmlFor="organization-title">Job Title</label>
-            <br /><input type="text" name="organization-title" required={true} autoComplete="organization-title" />
+            <br /><input type="text" name="organization-title" required={true} autoComplete="organization-title" title="Your job description this month 📝" />
           </fieldset>
           <fieldset>
             <legend>Contact Options</legend>
+            <br /><span>Note: Don't worry about the country code for phone numbers</span>
             <br /><label htmlFor="input-cell-phone">Cell Phone (optional)</label>
-            <br /><span>Note: Don't worry about the country code</span>
             <br /><input type="tel-national" name="input-cell-phone" autoComplete="tel-national" />
             <br /><label htmlFor="input-work-phone">Work Phone (optional)</label>
-            <br /><span>Note: Don't worry about the country code</span>
             <br /><input type="tel-national" name="input-work-phone" autoComplete="tel-national" />
             <br /><label htmlFor="input-email-address">Email Address</label>
-            <br /><input type="email" name="input-email-address" required={true} autoComplete="email" pattern="^[a-zA-Z0-9]+@[a-zA-Z0-9].awarehq.[a-zA-z]+$" title="Your @awarehq.com email please" />
+            <br /><input type="email" name="input-email-address" required={true} autoComplete="email" pattern="^[a-zA-Z0-9]+@[a-zA-Z0-9].awarehq.[a-zA-z]+$" title="Please use your @awarehq.com address" />
             <br /><label htmlFor="input-workplace-profile">Workplace Profile (optional)</label>
             <br /><span>Tip: Head to Workplace, go to your profile, and copy the URL</span>
             <br /><input type="url" name="input-workplace-profile" />

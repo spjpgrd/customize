6c018d05f666e2b647497c262231b7313df31627
Ringo Doran <sean.doran@wiretap.com>
2019-04-02 10:59:59 -0400

    ✨ Dirty datalist to add profile image

--- src/App.tsx
+++ src/App.tsx
@@ -54,217 +54,217 @@ class App extends React.Component<IAppProps, IAppState> {
       "fullName": "alex weiker",
       "fullNameSlug": "alex-weiker",
       "givenName": "alex",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/alex-weiker-64.png"
+      "profilePicture": "/profile-photos/alex-weiker-aware-64.png"
     }, {
       "familyName": "scarcello",
       "fullName": "andi scarcello",
       "fullNameSlug": "andi-scarcello",
       "givenName": "andi",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/andi-scarcello-64.png"
+      "profilePicture": "/profile-photos/andi-scarcello-aware-64.png"
     }, {
       "familyName": "dario",
       "fullName": "anthony dario",
       "fullNameSlug": "anthony-dario",
       "givenName": "anthony",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/anthony-dario-64.png"
+      "profilePicture": "/profile-photos/anthony-dario-aware-64.png"
     }, {
       "familyName": "mckinley",
       "fullName": "austin mckinley",
       "fullNameSlug": "austin-mckinley",
       "givenName": "austin",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/austin-mckinley-64.png"
+      "profilePicture": "/profile-photos/austin-mckinley-aware-64.png"
     }, {
       "familyName": "barnhouse",
       "fullName": "betsy barnhouse",
       "fullNameSlug": "betsy-barnhouse",
       "givenName": "betsy",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/betsy-barnhouse-64.png"
+      "profilePicture": "/profile-photos/betsy-barnhouse-aware-64.png"
     }, {
       "familyName": "sewell",
       "fullName": "betsy sewell",
       "fullNameSlug": "betsy-sewell",
       "givenName": "betsy",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/betsy-sewell-64.png"
+      "profilePicture": "/profile-photos/betsy-sewell-aware-64.png"
     }, {
       "familyName": "davell",
       "fullName": "cooper davell",
       "fullNameSlug": "cooper-davell",
       "givenName": "cooper",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/cooper-davell-64.png"
+      "profilePicture": "/profile-photos/cooper-davell-aware-64.png"
     }, {
       "familyName": "seibert",
       "fullName": "craig seibert",
       "fullNameSlug": "craig-seibert",
       "givenName": "craig",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/craig-seibert-64.png"
+      "profilePicture": "/profile-photos/craig-seibert-aware-64.png"
     }, {
       "familyName": "bourke",
       "fullName": "david bourke",
       "fullNameSlug": "david-bourke",
       "givenName": "david",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/david-bourke-64.png"
+      "profilePicture": "/profile-photos/david-bourke-aware-64.png"
     }, {
       "familyName": "moran",
       "fullName": "greg moran",
       "fullNameSlug": "greg-moran",
       "givenName": "greg",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/greg-moran-64.png"
+      "profilePicture": "/profile-photos/greg-moran-aware-64.png"
     }, {
       "familyName": "lawson",
       "fullName": "jack lawson",
       "fullNameSlug": "jack-lawson",
       "givenName": "jack",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/jack-lawson-64.png"
+      "profilePicture": "/profile-photos/jack-lawson-aware-64.png"
     }, {
       "familyName": "tsai",
       "fullName": "james tsai",
       "fullNameSlug": "james-tsai",
       "givenName": "james",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/james-tsai-64.png"
+      "profilePicture": "/profile-photos/james-tsai-aware-64.png"
     }, {
       "familyName": "morgan",
       "fullName": "jason morgan",
       "fullNameSlug": "jason-morgan",
       "givenName": "jason",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/jason-morgan-64.png"
+      "profilePicture": "/profile-photos/jason-morgan-aware-64.png"
     }, {
       "familyName": "stein",
       "fullName": "jason stein",
       "fullNameSlug": "jason-stein",
       "givenName": "jason",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/jason-stein-64.png"
+      "profilePicture": "/profile-photos/jason-stein-aware-64.png"
     }, {
       "familyName": "schumann",
       "fullName": "jeff schumann",
       "fullNameSlug": "jeff-schumann",
       "givenName": "jeff",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/jeff-schumann-64.png"
+      "profilePicture": "/profile-photos/jeff-schumann-aware-64.png"
     }, {
       "familyName": "spridgeon",
       "fullName": "jeff spridgeon",
       "fullNameSlug": "jeff-spridgeon",
       "givenName": "jeff",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/jeff-spridgeon-64.png"
+      "profilePicture": "/profile-photos/jeff-spridgeon-aware-64.png"
     }, {
       "familyName": "fletcher",
       "fullName": "julia fletcher",
       "fullNameSlug": "julia-fletcher",
       "givenName": "julia",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/julia-fletcher-64.png"
+      "profilePicture": "/profile-photos/julia-fletcher-aware-64.png"
     }, {
       "familyName": "debelak",
       "fullName": "kaitlyn debelak",
       "fullNameSlug": "kaitlyn-debelak",
       "givenName": "kaitlyn",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/kaitlyn-debelak-64.png"
+      "profilePicture": "/profile-photos/kaitlyn-debelak-aware-64.png"
     }, {
       "familyName": "ritzer",
       "fullName": "kristi ritzer",
       "fullNameSlug": "kristi-ritzer",
       "givenName": "kristi",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/kristi-ritzer-64.png"
+      "profilePicture": "/profile-photos/kristi-ritzer-aware-64.png"
     }, {
       "familyName": "vandemark",
       "fullName": "kristian vandemark",
       "fullNameSlug": "kristian-vandemark",
       "givenName": "kristian",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/kristian-vandemark-64.png"
+      "profilePicture": "/profile-photos/kristian-vandemark-aware-64.png"
     }, {
       "familyName": "keys",
       "fullName": "kyjah keys",
       "fullNameSlug": "kyjah-keys",
       "givenName": "kyjah",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/kyjah-keys-64.png"
+      "profilePicture": "/profile-photos/kyjah-keys-aware-64.png"
     }, {
       "familyName": "lescoezec",
       "fullName": "kyle lescoezec",
       "fullNameSlug": "kyle-lescoezec",
       "givenName": "kyle",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/kyle-lescoezec-64.png"
+      "profilePicture": "/profile-photos/kyle-lescoezec-aware-64.png"
     }, {
       "familyName": "huber",
       "fullName": "matt huber",
       "fullNameSlug": "matt-huber",
       "givenName": "matt",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/matt-huber-64.png"
+      "profilePicture": "/profile-photos/matt-huber-aware-64.png"
     }, {
       "familyName": "pasternack",
       "fullName": "matt pasternack",
       "fullNameSlug": "matt-pasternack",
       "givenName": "matt",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/matt-pasternack-64.png"
+      "profilePicture": "/profile-photos/matt-pasternack-aware-64.png"
     }, {
       "familyName": "stiffler",
       "fullName": "matt stiffler",
       "fullNameSlug": "matt-stiffler",
       "givenName": "matt",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/matt-stiffler-64.png"
+      "profilePicture": "/profile-photos/matt-stiffler-aware-64.png"
     }, {
       "familyName": "mcclellan",
       "fullName": "megan mcclellan",
       "fullNameSlug": "megan-mcclellan",
       "givenName": "megan",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/megan-mcclellan-64.png"
+      "profilePicture": "/profile-photos/megan-mcclellan-aware-64.png"
     }, {
       "familyName": "perdue",
       "fullName": "missy perdue",
       "fullNameSlug": "missy-perdue",
       "givenName": "missy",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/missy-perdue-64.png"
+      "profilePicture": "/profile-photos/missy-perdue-aware-64.png"
     }, {
       "familyName": "holmer",
       "fullName": "nick holmer",
       "fullNameSlug": "nick-holmer",
       "givenName": "nick",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/nick-holmer-64.png"
+      "profilePicture": "/profile-photos/nick-holmer-aware-64.png"
     }, {
       "familyName": "dunewood",
       "fullName": "robb dunewood",
       "fullNameSlug": "robb-dunewood",
       "givenName": "robb",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/robb-dunewood-64.png"
+      "profilePicture": "/profile-photos/robb-dunewood-aware-64.png"
     }, {
       "familyName": "kenny",
       "fullName": "sarah kenny",
       "fullNameSlug": "sarah-kenny",
       "givenName": "sarah",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/sarah-kenny-64.png"
+      "profilePicture": "/profile-photos/sarah-kenny-aware-64.png"
     }, {
       "familyName": "O'Neill",
       "fullName": "Sean O'Neill",
       "fullNameSlug": "sean-oneill",
       "givenName": "Sean",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/sean-oneill-64.png"
+      "profilePicture": "/profile-photos/sean-oneill-aware-64.png"
     }, {
       "familyName": "doran",
       "fullName": "sean patrick john paul george ringo doran",
       "fullNameSlug": "sean-patrick-john-paul-george-ringo-doran",
       "givenName": "sean",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/sean-patrick-john-paul-george-ringo-doran-64.png"
+      "profilePicture": "/profile-photos/sean-patrick-john-paul-george-ringo-doran-aware-64.png"
     }, {
       "familyName": "domer",
       "fullName": "shawn domer",
       "fullNameSlug": "shawn-domer",
       "givenName": "shawn",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/shawn-domer-64.png"
+      "profilePicture": "/profile-photos/shawn-domer-aware-64.png"
     }, {
       "familyName": "ruland",
       "fullName": "steve ruland",
       "fullNameSlug": "steve-ruland",
       "givenName": "steve",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/steve-ruland-64.png"
+      "profilePicture": "/profile-photos/steve-ruland-aware-64.png"
     }, {
       "familyName": "smallwood",
       "fullName": "susan smallwood",
       "fullNameSlug": "susan-smallwood",
       "givenName": "susan",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/susan-smallwood-64.png"
+      "profilePicture": "/profile-photos/susan-smallwood-aware-64.png"
     }, {
       "familyName": "shen",
       "fullName": "xu joseph shen",
       "fullNameSlug": "xu-joseph-shen",
       "givenName": "xu",
-      "profilePicture": "https://www.awarehq.com/hubfs/email-signature/aware/xu-joseph-shen-64.png"
+      "profilePicture": "/profile-photos/xu-joseph-shen-aware-64.png"
     },
     ];
 
@@ -335,11 +335,6 @@ class App extends React.Component<IAppProps, IAppState> {
     await this.setState({
       [name]: value,
     });
-    const givenName = value.givenName;
-    const familyName = value.familyName;
-    const fullNameSlug = value.fullNameSlug;
-    const profilePicture = value.profilePicture;
-    this.setState({ givenName, familyName, fullNameSlug, profilePicture });
   }
 
   // Probably wont work for easily autopopulating the profile picture
@@ -358,21 +353,21 @@ class App extends React.Component<IAppProps, IAppState> {
         <div className="row between-xs">
           <div className="last-xs last-sm initial-order-md col-xs-12 col-sm-12 col-md-6 col-lg-5 col-xl-4">
             <form name="personal-info" autoComplete="on">
-              {/* <fieldset>
-                <legend>Quick Setup</legend>
-                <label htmlFor="employeeSelected">What's your name?</label>
-                <input type="text" list="employeeList" id="employeeSelected" name="employeeSelected" onChange={this.handleEmployeeListSelection} autoFocus={true} />
+              <fieldset>
+                <legend>Pick Your Pic</legend>
+                <label htmlFor="profilePicture">Browse or type to search</label>
+                <input type="text" list="employeeList" id="profilePicture" name="profilePicture" onChange={this.handleEmployeeListSelection} autoFocus={true} />
                 <datalist id="employeeList" >
                   {this.state.employeeList.map((employee, index) => (
-                    <option key={index} value={employee.fullName}>{employee.fullName}</option>
+                    <option key={index} value={employee.profilePicture}>{employee.profilePicture}</option>
                   ))}
                 </datalist>
-              </fieldset> */}
-              <fieldset>
+              </fieldset>
+              {/* <fieldset>
                 <legend>Your Photo</legend>
                 <label htmlFor="profilePicture">Profile Picture URL</label><br />
                 <input type="photo" name="profilePicture" value={this.state.profilePicture || ""} required={true} onChange={this.handleInputChange} /><br />
-              </fieldset>
+              </fieldset> */}
               <fieldset>
                 <legend>Your Details</legend>
                 <label htmlFor="honorificPrefix">Honorific Prefix <span className="c-form-label__optional">(optional)</span></label><br />

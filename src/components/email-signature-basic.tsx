2c7f5117282fca36b20b59039e41f92f1c6e3631
Ringo Doran <sean.doran@wiretap.com>
2019-04-02 09:40:35 -0400

    🐒 Funky twerking on line breaks

--- src/components/email-signature-basic.tsx
+++ src/components/email-signature-basic.tsx
@@ -46,17 +46,17 @@ class EmailSignatureBasic extends React.Component<IEmailSignatureBasicProps, IEm
     public render() {
         return (
             <>
-                <table cellSpacing="0" cellPadding="0" style={{ border: "0px", borderSpacing: "0px" }}>
+                <table cellSpacing="0" cellPadding="0" style={{ border: "0px" }}>
                     <tbody>
-                        <tr style={{ paddingLeft: "0px", paddingTop: "0px", paddingBottom: "0px", paddingRight: "0px", marginTop: "0px", borderCollapse: "collapse", border: "0px" }}>
+                        <tr >
                             <td style={{ paddingLeft: "0px", paddingTop: "32px", paddingBottom: " 4px", paddingRight: "0px", verticalAlign: "top" }}>
                                 <a href={this.props.linkedinUrl}><img src={this.props.profilePicture} style={{ width: "64px", height: "64px", }} width="64" height="64" alt={this.props.fullName} title={this.props.fullName} /></a></td>
                         </tr>
-                        <tr style={{ paddingLeft: "0px", paddingTop: "0px", paddingBottom: "0px", paddingRight: "0px", marginTop: "0px", borderCollapse: "collapse", border: "0px" }}>
+                        <tr>
                             <td style={{ verticalAlign: "top", paddingLeft: "0px", paddingTop: "0px", paddingBottom: "8px", paddingRight: "0px" }}>
                                 <span style={{ textAlign: "left", color: "#2C2B2A", fontFamily: "'Effra','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontWeight: "bold" }}>{this.props.fullName}<br /><span style={{ textAlign: "left", marginTop: "0px", marginBottom: "0px", color: "#3F3D3C", fontFamily: "'Effra','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontWeight: "normal", fontSize: "10pt", lineHeight: "1.5" }}>{this.props.organizationTitle}<span style={{ color: "#878582", fontVariantNumeric: "tabular-nums slashed-zero", }}>{this.props.workPhone ? <span><br />O: {this.props.workPhone}</span> : ""}{this.props.cellPhone ? <span><br />C: {this.props.cellPhone}</span> : ""}</span><br /><a style={{ textDecoration: "none", borderBottomWidth: "0", color: "#E64257", fontFamily: "'Effra','-apple-system', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial','sans-serif','Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','sans-serif'", fontSize: "10pt", fontStyle: "normal", fontWeight: "normal", lineHeight: "1.5" }} href={"mailto:" + this.props.emailAddress}>{this.props.emailAddress}</a></span></span></td>
                         </tr>
-                        <tr style={{ paddingLeft: "0px", paddingTop: "0px", paddingBottom: "0px", paddingRight: "0px", marginTop: "0px", borderCollapse: "collapse", border: "0px" }}>
+                        <tr>
                             <td style={{ verticalAlign: "bottom", paddingLeft: "0px", paddingTop: "16px", paddingBottom: "0px", paddingRight: "0px", marginTop: "0px" }}>
                                 <a href={"https://awarehq.com?utm_medium=" + this.props.utmParams.utmMedium + "?utm_source=" + this.props.utmParams.utmSource + "?utm_content=" + this.props.fullNameSlug + "-aware-brand-launch"}><img src="https://www.awarehq.com/hubfs/email-signature/aware/aware-email-32.png" style={{ width: "128px", height: "32px", border: "0" }} width="128" height="32" alt="Aware Logo" title="Aware Logo" /></a>
                             </td>
